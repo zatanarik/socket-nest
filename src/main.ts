883e8606a90e2ecf945f36f7014c97d898e4c191
Igor <zatanarik@gmail.com>
2024-07-02 17:10:42 +0300

    refactor: message after the server start

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const API_PORT = process.env.API_PORT || 4200;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(API_PORT).then(() => {
+    console.log(`Server started on port: ${API_PORT}`);
+  });
 }
 bootstrap();
